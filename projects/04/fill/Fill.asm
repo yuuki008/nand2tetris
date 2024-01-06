624096b685ed0856f535b90924d80116ab2e2cdc
yuuki008 <s22ah261@gmail.com>
2024-01-06 11:07:06 +0900

    Fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,60 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 //// Replace this comment with your code.
+
+// キーボード監視
+    @24576
+    D=A
+    @keyboard_start
+    M=D
+
+(KEYBOARD_EVENT_HANDLER)
+    @keyboard_start
+    D=M
+
+    @BLACK_SCREEN
+    D;JNE
+
+    @KEYBOARD_EVENT_HANDLER
+    0;JMP
+(KEYBOARD_END)
+
+// スクリーン制御
+    @24576
+    D=A
+    @screen_end
+    M=D
+
+    @16384
+    D=A
+    @screen_start
+    M=D
+
+    @i
+    M=0
+
+(BLACK_SCREEN)
+    @i
+    D=M
+    @screen-end
+    D=D-M
+    @screen_start
+    D=D-M
+    @BLACK_SCREEN_END
+    D;JEQ
+
+    @screen_start
+    D=M
+    @i
+    A=D+M
+    M=-1
+
+    @i
+    M=M+1
+
+    @BLACK_SCREEN
+    0;JMP
+
+(BLACK_SCREEN_END)
+    @BLACK_SCREEN_END
+    0;JMP
