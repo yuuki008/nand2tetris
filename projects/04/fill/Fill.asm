a80edb3f71117ae9af075fe5bcd69e15cd936c7c
yuuki008 <s22ah261@gmail.com>
2024-01-06 12:03:44 +0900

    Fill お手本

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -4,68 +4,58 @@
 // File name: projects/04/Fill.asm
 
 // Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen
-// by writing 'black' in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen by writing
-// 'white' in every pixel;
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed.
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-//// Replace this comment with your code.
-
-// キーボード監視
-    @24576
-    D=A
-    @keyboard_start
-    M=D
-
-(KEYBOARD_EVENT_HANDLER)
-    @keyboard_start
-    D=M
-
-    @BLACK_SCREEN
-    D;JNE
-
-    @KEYBOARD_EVENT_HANDLER
-    0;JMP
-(KEYBOARD_END)
-
-// スクリーン制御
-    @24576
-    D=A
-    @screen_end
-    M=D
-
-    @16384
-    D=A
-    @screen_start
-    M=D
-
-    @i
-    M=0
-
-(BLACK_SCREEN)
-    @i
-    D=M
-    @screen-end
-    D=D-M
-    @screen_start
-    D=D-M
-    @BLACK_SCREEN_END
-    D;JEQ
-
-    @screen_start
-    D=M
-    @i
-    A=D+M
-    M=-1
-
-    @i
-    M=M+1
-
-    @BLACK_SCREEN
+// Put your code here.
+    @status
+    M=-1        // status=0xFFFF
+    D=0         // Argument - what to set screen bits to
+    @SETSCREEN
     0;JMP
 
-(BLACK_SCREEN_END)
-    @BLACK_SCREEN_END
+(LOOP)
+    @KBD // 定義済みのキーボードの値が入っているアドレス
+    D=M  // D = キーボードの文字
+    @SETSCREEN
+    D;JEQ // キーが押されていなければゼロを表示
+    D=-1 // キーが押されていれば全て1を表示
+
+(SETSCREEN)
+    @ARG // キーが押されていれば文字、押されていなければ-1が格納されたアドレス
+    M=D // 新しい状態
+    @status // キーが入ったアドレス
+    D=D-M
+    @LOOP
+    D;JEQ // 新旧のステータスを比較し同じならば再度ループ
+
+    @ARG
+    D=M // キーの文字を取得
+    @status
+    M=D // キーのを納
+
+    @SCREEN // D = スクリーンのアドレス
+    D=A // 8Kのメモリマップ 32*256
+    @8192 // D = スクリーンのアドレス
+    D=D+A
+    @i
+    M=D // i = スクリーンのアドレス
+
+(SETLOOP)
+    @i
+    D=M-1
+    M=D // スクリーンのアドレス - 1
+    @LOOP
+    D;JLT
+
+    @status
+    D=M
+    @i
+    A=M
+    M=D
+    @SETLOOP
     0;JMP
