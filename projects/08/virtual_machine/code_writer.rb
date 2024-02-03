fae3e19d4a338c0528a19dd1ac1cba951d3126a5
yuuki008 <s22ah261@gmail.com>
2024-02-03 12:02:38 +0900

    07のvirtual_machineを08にコピー

---
+++ projects/08/virtual_machine/code_writer.rb
@@ -0,0 +1,281 @@
+class CodeWriter
+  REFERENCED_SEGMENTS = {
+    "local" => "LCL",
+    "argument" => "ARG",
+    "this" => "THIS",
+    "that" => "THAT",
+  }.freeze
+
+  STATIC_SEGMENTS = {
+    "pointer" => 3,
+    "temp" => 5,
+  }.freeze
+
+  def initialize
+    @output_file = nil
+    @label_count = 0
+    @set_file_name = nil
+  end
+
+  def set_file_name(file_name)
+    @set_file_name = File.basename(file_name, File.extname(file_name))
+    output_file_path = file_name.gsub(/\.vm$/, ".asm")
+    @output_file = File.open(output_file_path, "w")
+  end
+
+  def write_arithmetic(command)
+    case command
+    when 'add', 'sub', 'and', 'or'
+      write_binary_operation(command)
+    when "neg", 'not'
+      write_unary_operation(command)
+    when "eq", "gt", "lt"
+      write_comp_operation(command)
+    end
+  end
+
+  def write_push_pop(command, segment, index) 
+    case command
+    when "C_PUSH"
+      write_push(segment, index)
+    when "C_POP"
+      write_pop(segment, index)
+    end
+  end
+
+  private
+
+  def write_push(segment, index)
+    case
+    when REFERENCED_SEGMENTS.keys.include?(segment)
+      write_push_from_referenced_segment(segment, index)
+    when STATIC_SEGMENTS.keys.include?(segment)
+      write_push_from_static_segment(segment, index)
+    when segment == "static" 
+      write_push_from_static(segment, index)
+    when segment == "constant"
+      write_push_from_constant_segment(segment, index)
+    end
+  end
+
+  def write_push_from_referenced_segment(segment, index)
+    commands = []
+    commands << "@#{REFERENCED_SEGMENTS[segment]}"
+    commands << "A=M"
+    for i in 0..index.to_i - 1
+      commands << "A=A+1"
+    end
+    commands << "D=M"
+    commands << "@SP"
+    commands << "A=M"
+    commands << "M=D"
+    commands << "@SP"
+    commands << "M=M+1"
+
+    write_commands(commands)
+  end
+
+  def write_push_from_static_segment(segment, index)
+    commands = []
+    commands << "@#{STATIC_SEGMENTS[segment]}"
+    for i in 0..index.to_i - 1
+      commands << "A=A+1"
+    end
+    commands << "D=M"
+    commands << "@SP"
+    commands << "A=M"
+    commands << "M=D"
+    commands << "@SP"
+    commands << "M=M+1"
+
+    write_commands(commands)
+  end
+
+  def write_push_from_static(segment, index)
+    commands = []
+    commands << "@#{@set_file_name}.#{index}"
+    commands << "D=M"
+    commands << "@SP"
+    commands << "A=M"
+    commands << "M=D"
+    commands << "@SP"
+    commands << "M=M+1"
+
+    write_commands(commands)
+  end
+
+  def write_push_from_constant_segment(segment, index)
+    commands = []
+    commands << "@#{index}"
+    commands << "D=A"
+    commands << "@SP"
+    commands << "A=M"
+    commands << "M=D"
+    commands << "@SP"
+    commands << "M=M+1"
+
+    write_commands(commands)
+  end
+
+  def write_pop(segment, index)
+    case
+    when REFERENCED_SEGMENTS.keys.include?(segment)
+      write_pop_from_referenced_segment(segment, index)
+    when STATIC_SEGMENTS.keys.include?(segment)
+      write_pop_from_static_segment(segment, index)
+    when segment == "static"
+      write_pop_from_static(segment, index)
+    when segment == "constant"
+      write_pop_from_constant_segment(segment, index)
+    end
+  end
+
+  def write_pop_from_referenced_segment(segment, index)
+    commands = []
+    commands << "@SP"
+    commands << "M=M-1"
+    commands << "A=M"
+    commands << "D=M"
+    commands << "@#{REFERENCED_SEGMENTS[segment]}"
+    commands << "A=M"
+    for i in 0..index.to_i - 1
+      commands << "A=A+1"
+    end
+    commands << "M=D"
+
+    write_commands(commands)
+  end
+
+  def write_pop_from_static_segment(segment, index)
+    commands = []
+    commands << "@SP"
+    commands << "M=M-1"
+    commands << "A=M"
+    commands << "D=M"
+    commands << "@#{STATIC_SEGMENTS[segment]}"
+    for i in 0..index.to_i - 1
+      commands << "A=A+1"
+    end
+    commands << "M=D"
+
+    write_commands(commands)
+  end
+
+  def write_pop_from_static(segment, index)
+    commands = []
+    commands << "@SP"
+    commands << "M=M-1"
+    commands << "A=M"
+    commands << "D=M"
+    commands << "@#{@set_file_name}.#{index}"
+    commands << "M=D"
+
+    write_commands(commands)
+  end
+
+  def write_pop_from_constant_segment(segment, index)
+    commands = []
+    commands << "@SP"
+    commands << "M=M-1"
+    commands << "A=M"
+    commands << "D=M"
+    commands << "@#{index}"
+    commands << "M=D"
+    write_commands(commands)
+  end
+
+  def write_binary_operation(command)
+    write_pop_to_a_register
+    write_command('D=M')
+    write_pop_to_a_register
+
+    case command
+    when "add"
+      write_command('D=D+M')
+    when 'sub'
+      write_command('D=M-D')
+    when 'and'
+      write_command('D=D&M')
+    when 'or'
+      write_command('D=D|M')
+    end
+    write_push_from_d_register
+  end
+
+  def write_unary_operation(command)
+    write_commands([
+      '@SP',
+      'A=M-1',
+    ])
+
+    case command
+    when "neg"
+      write_command('M=-M')
+    when "not"
+      write_command('M=!M')
+    end
+  end
+
+  def write_comp_operation(command)
+    write_pop_to_a_register
+    write_command('D=M')
+    write_pop_to_a_register
+    l1 = new_label
+    l2 = new_label
+
+    case command
+    when "eq"
+      com_type = "JEQ"
+    when "gt"
+      comp_type = "JGT"
+    when "lt"
+      comp_type = "JLT"
+    end
+
+    write_commands([
+      "D=M-D",
+      "@#{l1}",
+      "D;#{comp_type}",
+      "D=0",
+      "@#{l2}",
+      "0;JMP",
+      "(#{l1})",
+      "D=-1",
+      "(#{l2})",
+    ])
+    write_push_from_d_register
+  end
+
+  def write_pop_to_a_register
+    write_commands([
+      '@SP',
+      'M=M-1',
+      'A=M'
+    ])
+  end
+
+  def write_push_from_d_register
+    write_commands([
+      '@SP',
+      'A=M',
+      'M=D',
+      '@SP',
+      'M=M+1'
+    ])
+  end
+
+  def write_command(command)
+    @output_file.puts(command)
+  end
+
+  def write_commands(commands)
+    commands.each do |command|
+      @output_file.puts(command)
+    end
+  end
+
+  def new_label
+    @label_count += 1
+    "LABEL_#{@label_count}"
+  end
+end
---
+++ projects/08/virtual_machine/parser.rb
@@ -0,0 +1,67 @@
+class Parser
+  def initialize(file_path)
+    lines = file_readlines(file_path)
+    @commands = lines.reject { |line| line.include?("//") || line.strip.empty? }
+    @index = 0
+  end
+
+  def has_more_commands
+    @index < @commands.length
+  end
+
+  def advance
+    @index += 1
+  end
+
+  def command_type
+    command = @commands[@index].strip
+
+    case
+    when command.include?("push")
+      return "C_PUSH"
+    when command.include?("pop")
+      return "C_POP"
+    when command.include?("label")
+      return "C_LABEL"
+    when command.include?("goto")
+      return "C_GOTO"
+    when command.include?("if-goto")
+      return "C_IF"
+    when command.include?("function")
+      return "C_FUNCTION"
+    when command.include?("call")
+      return "C_CALL"
+    when command.include?("return")
+      return "C_RETURN"
+    else
+      return "C_ARITHMETIC"
+    end
+  end
+
+  def arg1
+    command_words = @commands[@index].strip.split(" ")
+    command_words[1] || command_words[0]
+  end
+
+  def arg2
+    @commands[@index].strip.split(" ")[2]
+  end
+
+  private 
+  def file_readlines(file_path)
+    if File.extname(file_path) != ".vm"
+      puts "エラー: 指定されたファイルはVMファイルではありません。プログラムを中断します。"
+      exit 1 
+    end
+
+    begin
+      File.readlines(file_path)
+    rescue Errno::ENOENT
+      puts "エラー: 指定されたファイルが見つかりませんでした。プログラムを中断します。"
+      exit 1 
+    rescue => e
+      puts "エラーが発生しました: #{e.message}プログラムを中断します。"
+      exit 1 
+    end
+  end
+end
