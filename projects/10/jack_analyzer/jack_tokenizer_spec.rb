dc06a5981244ed4c6f7529e068c2d4f239df841a
yuuki008 <s22ah261@gmail.com>
2024-02-17 12:37:57 +0900

    feat: テスト作成

---
+++ projects/10/jack_analyzer/jack_tokenizer_spec.rb
@@ -0,0 +1,68 @@
+require_relative 'jack_tokenizer'
+require 'minitest/autorun'
+
+class JackTokenizerTest < Minitest::Test
+  describe "token_type" do
+    def test_keyword
+      tokenizer = JackTokenizer.new("./test.jack")
+
+      JackTokenizer::KEYWORDS.each do |key, value|
+        assert_equal "KEYWORD", tokenizer.token_type(key)
+      end
+    end
+
+    def test_symbol
+      tokenizer = JackTokenizer.new("./test.jack")
+
+      JackTokenizer::SYMBOLS.each do |symbol|
+        assert_equal "SYMBOL", tokenizer.token_type(symbol)
+      end
+    end
+
+    def test_int_const
+      tokenizer = JackTokenizer.new("./test.jack")
+
+      assert_equal "INT_CONST", tokenizer.token_type("123")
+      assert_equal "INT_CONST", tokenizer.token_type("0")
+    end
+
+    def test_string_const
+      tokenizer = JackTokenizer.new("./test.jack")
+
+      assert_equal "STRING_CONST", tokenizer.token_type('"Hello"')
+    end
+
+    def test_identifier
+      tokenizer = JackTokenizer.new("./test.jack")
+
+      assert_equal "IDENTIFIER", tokenizer.token_type('hoge')
+    end
+  end
+
+  describe 'advance' do
+    def test_advance
+      tokenizer = JackTokenizer.new("./test.jack")
+
+      file = File.open("./test.jack")
+      tokens = file.read.split(/(\s+|\{|}|\(|\)|\[|\]|\.|,|;|\+|-|\*|\/|&|\||<|>|=|~)/).reject { |token| token.strip.empty? }
+
+      tokens.each do |token, index|
+        assert_equal token, tokens[tokenizer.index]
+        tokenizer.advance
+      end
+    end
+  end
+
+  describe 'execute' do
+    def test_output_xml
+      tokenizer = JackTokenizer.new("./test.jack")
+      tokenizer.execute
+
+      file = File.open("./testT.xml")
+      expected = file.read
+      actual = File.open(tokenizer.output_file.path).read
+
+      assert_equal actual, expected
+    end
+  end
+end
