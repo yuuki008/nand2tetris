b011dba8972cb90003f1a3e20707427b00e5cfd8
yuuki008 <s22ah261@gmail.com>
2024-02-29 20:06:10 +0900

    vm_writer とりあえず

--- projects/11/jack_compiler/vm_writer.rb
+++ projects/11/jack_compiler/vm_writer.rb
@@ -1,3 +1,45 @@
 class VmWriter
-  
+  def initialize(output_file_path)
+    @output_file = File.open(output_file_path, 'w')
+  end
+
+  def write_push(segment, index)
+    @output_file.puts("push #{segment} #{index}")
+  end
+
+  def write_pop(segment, index)
+    @output_file.puts("pop #{segment} #{index}")
+  end
+
+  def write_arithmetic(command)
+    @output_file.puts(command)
+  end
+
+  def write_label(label)
+    @output_file.puts("label #{label}")
+  end
+
+  def write_goto(label)
+    @output_file.puts("goto #{label}")
+  end
+
+  def write_if(label)
+    @output_file.puts("if-goto #{label}")
+  end
+
+  def write_call(name, n_args)
+    @output_file.puts("call #{name} #{n_args}")
+  end
+
+  def write_function(name, n_locals)
+    @output_file.puts("function #{name} #{n_locals}")
+  end
+
+  def write_return
+    @output_file.puts("return")
+  end
+
+  def close
+    @output_file.close
+  end
 end
